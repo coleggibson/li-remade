1b770ceac6b14faedead1ec683ff8de89b549fc6
Cole Gibson <coleggibson@gmail.com>
2023-03-01 11:01:06 -0600

    Added icons and spacing for submit bar

--- src/components/submitbar.js
+++ src/components/submitbar.js
@@ -1,13 +1,26 @@
+import '../style/submitbar.css'
+import {calendar, gallery, newspaper, videoIcon} from '../images/index'
+
 const SubmitBar = () => {
     return (
         <div id='submit-section' className='section middle-column'>
-            <div>profile pic</div>
-            <input type='text' placeholder='Start a post'/>
-            <div id='submit-options'>
-                <div className='submit-button'>Photo</div>
-                <div className='submit-button'>Video</div>
-                <div className='submit-button'>Event</div>
-                <div className='submit-button'>Write article</div>
+            <div id='submit-bar-top' className='submit-subsection'>
+                <div>profile pic</div>
+                <input id='submit-input' type='text' placeholder='Start a post'/>
+            </div>
+            <div id='submit-bar-bottom' className='submit-subsection'>
+                <div className='submit-button'>
+                <img className='submit-bar-icon' src={gallery}></img>
+                </div>
+                <div className='submit-button'>
+                <img className='submit-bar-icon' src={videoIcon}></img>
+                </div>
+                <div className='submit-button'>
+                <img className='submit-bar-icon' src={calendar}></img>
+                </div>
+                <div className='submit-button'>
+                <img className='submit-bar-icon' src={newspaper}></img>
+                </div>
             </div>
         </div>
     )
