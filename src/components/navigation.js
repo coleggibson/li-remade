1b89c60b4fada2ef2290d09396c1a3827566ce5f
Cole Gibson <coleggibson@gmail.com>
2023-03-01 10:37:39 -0600

    Added images into nav component and adjusted spacing

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,38 +1,42 @@
 import '../style/navigation.css'
 
 // import '../images/index'
-import {logo} from '../images/index'
+import {bell, logo, target, workIcon} from '../images/index'
 import {homeIcon} from '../images/index.js'
+import { briefcase } from '../images/index'
+import { messageBubble } from '../images/index'
+import { peopleIcon } from '../images/index'
+
 
 const Navigation = () => {
     return(
     <div id='navigation-section' className ='section'>
-        <img src={logo} alt='logo'/>
+        <img className='nav-image' src={logo} alt='logo'/>
         <input type='text' placeholder='Search Bar'/>
         <div id='right-nav-items'>
             <div className='nav-button'>
-                <img src={homeIcon}/>
+                <img className='nav-image' src={homeIcon}/>
             </div>
             <div className='nav-button'>
-                <div>My Network</div>
+            <img className='nav-image' src={peopleIcon}/>
             </div>
             <div className='nav-button'>
-                <div>Jobs</div>
+            <img className='nav-image' src={briefcase}/>
             </div>
             <div className='nav-button'>
-                <div>Messaging</div>
+            <img className='nav-image' src={messageBubble}/>
             </div>
             <div className='nav-button'>
-                <div>Notifications</div>
+            <img className='nav-image' src={bell}/>
             </div>
             <div className='nav-button'>
                 <div>Me</div>
             </div>
             <div className='nav-button'>
-                <div>Work</div>
+            <img className='nav-image' src={workIcon}/>
             </div>
             <div className='nav-button'>
-                <div>Advertise</div>
+            <img className='nav-image' src={target}/>
             </div>
         </div>
     </div>
--- src/images/index.js
+++ src/images/index.js
@@ -5,20 +5,20 @@
 
 
 
-const workIcon = 'src/images/9dots.png'
-const bell = 'src/images/bell.png'
-const briefcase = 'src/images/briefcase.png'
-const calendar = 'src/images/calendar.png'
-const gallery ='src/images/gallery.png'
+export const workIcon = require('./9dots.png')
+export const bell = require ('./bell.png')
+export const briefcase = require('./briefcase.png')
+export const calendar = require('./calendar.png')
+export const gallery =require('./gallery.png')
 export const homeIcon = require('./homeicon.png')
 export const logo = require('./LIlogo.png')
-const messageBubble = 'src/images/message.png'
-const newspaper = 'src/images/newspaper.png'
-const peopleIcon = 'src/images/people.png'
-const target = 'src/images/target.png'
-const videoIcon = 'src/images/video.png'
-const person1 = 'src/images/people.png'
-const person2 = 'src/images/pexels-italo-melo-2379004.jpg'
-const person3 = 'src/images/pexels-pixabay-220453.jpg'
+export const messageBubble = require('./message.png')
+export const newspaper = 'src/images/newspaper.png'
+export const peopleIcon = require('./people.png')
+export const target = require('./target.png')
+export const videoIcon = 'src/images/video.png'
+export const person1 = 'src/images/people.png'
+export const person2 = 'src/images/pexels-italo-melo-2379004.jpg'
+export const person3 = 'src/images/pexels-pixabay-220453.jpg'
 
 
